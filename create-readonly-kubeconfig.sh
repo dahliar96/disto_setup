30afb792f670cae81ab68586f33e93456541a485
DistoBot <distobot@distoai.com>
2023-09-03 18:47:15 +0100

    updating script with direct secret creation

--- create-readonly-kubeconfig.sh
+++ create-readonly-kubeconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create service account
-kubectl create serviceaccount disto-readonly
+kubectl -n default create serviceaccount disto-readonly
 
 # Create cluster role
 kubectl create clusterrole disto-readonly --verb=get,list,watch --resource="*"
@@ -9,14 +9,26 @@ kubectl create clusterrole disto-readonly --verb=get,list,watch --resource="*"
 # Create cluster role binding
 kubectl create clusterrolebinding disto-readonly --clusterrole=disto-readonly --serviceaccount=default:disto-readonly
 
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: disto-readonly-secret
+  namespace: default
+  annotations:
+    kubernetes.io/service-account.name: disto-readonly
+type: kubernetes.io/service-account-token
+EOF
+
 # Get secret name
-SECRET_NAME=$(kubectl get serviceaccount disto-readonly -n default -o jsonpath='{.secrets[0].name}')
+#SECRET_NAME=$(kubectl get serviceaccount disto-readonly -n default -o jsonpath='{.secrets[0].name}')
+SECRET_NAME=disto-readonly-secret
 
 # Get token
 TOKEN=$(kubectl get secret $SECRET_NAME -n default -o jsonpath='{.data.token}' | base64 --decode)
 
 # Get CA certificate
-CA_CERTIFICATE=$(kubectl get secret $SECRET_NAME -n default -o jsonpath='{.data.ca.crt}')
+CA_CERTIFICATE=$(kubectl get secret $SECRET_NAME -n default -o jsonpath='{.data.ca\.crt}')
 
 # Get API server
 APISERVER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
